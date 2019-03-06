76ff917b1c43f28c8bc3998841e6c95efec6a9ee
kyL_lee <leekwanyao710@gmail.com>
2019-03-06 11:41:46 +0800

    Update test0306.java

--- test0306.java
+++ test0306.java
@@ -7,14 +7,14 @@ public class test0306 {
         sc.nextLine();sc.nextLine();
         for(int i=0;i<count;i++){
         	char [] num = sc.nextLine().toCharArray();
-			int a = 0,b = 0;
-			for(int j=0;j<num.length;j++){
-				if(num[j]=='O')
-					a+=++b;
-				if(num[j]=='X')
-					b = 0;
-			}
-			System.out.println(a);
+		int a = 0,b = 0;
+		for(int j=0;j<num.length;j++){
+			if(num[j]=='O')
+				a+=++b;
+			if(num[j]=='X')
+				b = 0;
+		}
+		System.out.println(a);
         }
     }
 }
