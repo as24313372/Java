42410c701790a14ad75946eb501113f94e277c14
kyL_lee <leekwanyao710@gmail.com>
2019-03-06 11:41:25 +0800

    Update test0306.java

--- test0306.java
+++ test0306.java
@@ -3,14 +3,18 @@ import java.util.*;
 public class test0306 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	char [] num = sc.nextLine().toCharArray();
-	int a = 0,b = 0;
-	for(int j=0;j<num.length;j++){
-		if(num[j]=='O')
-			a+=++b;
-		if(num[j]=='X')
-			b = 0;
-	}
-	System.out.println(a);
+        int count = sc.nextInt();
+        sc.nextLine();sc.nextLine();
+        for(int i=0;i<count;i++){
+        	char [] num = sc.nextLine().toCharArray();
+			int a = 0,b = 0;
+			for(int j=0;j<num.length;j++){
+				if(num[j]=='O')
+					a+=++b;
+				if(num[j]=='X')
+					b = 0;
+			}
+			System.out.println(a);
+        }
     }
 }
