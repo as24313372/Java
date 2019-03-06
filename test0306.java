1f981731ee4ffa89c21c61e5e4f72f1dc59681ab
kyL_lee <leekwanyao710@gmail.com>
2019-03-06 11:26:09 +0800

    Update test0306.java

--- test0306.java
+++ test0306.java
@@ -3,17 +3,14 @@ import java.util.*;
 public class test0306 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		char [] num = sc.nextLine().toCharArray();
-		int a = 0,b = 0;
-		for(int j=0;j<num.length;j++){
-			if(num[j]=='O')
-				a+=++b;
-			if(num[j]=='X')
-				b = 0;
-		}
-		sop(a + "\n");
-    }
-    static void sop(String s){
-    	System.out.print(s);
+	char [] num = sc.nextLine().toCharArray();
+	int a = 0,b = 0;
+	for(int j=0;j<num.length;j++){
+		if(num[j]=='O')
+			a+=++b;
+		if(num[j]=='X')
+			b = 0;
+	}
+	System.out.println(a);
     }
 }
