a90ec2fa1d022496a2de5aec192c1ff40c05b4cd
kyL_lee <leekwanyao710@gmail.com>
2019-03-27 12:02:31 +0800

    Update test03273.java

--- test03273.java
+++ test03273.java
@@ -5,8 +5,8 @@ public class test03273 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        float p = 1;
-        p/=6;float q =1-p;
+        float p =(float)1/6;
+        float q =1-p;
         float change = (float)(p * Math.pow(q,k-1)/(1-Math.pow(q,n)));
         System.out.printf("%.4f",change);
     }
