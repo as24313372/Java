3804b4d1e9de1c8392a08cec3dc7094a87ccb74b
kyL_lee <leekwanyao710@gmail.com>
2019-04-03 11:14:47 +0800

    Update test0403.java

--- test0403.java
+++ test0403.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class test0403 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char num [] = sc.nextLine().split(" ").toCharArray();
-
- 
+        String cardnum = sc.nextLine().replace(" ","");
+        char num [] = cardnum.toCharArray();
         int count = 0;
         for(int i=0;i<num.length;i++){
         	if(i%2==0){
         		int a = num[i] - '0';
         		a *= 2;
-        		if(a/10!=0){//¤G¦ì¼Æ
-        			count += a/10;
-        			count += a%10;
-        		}else{
-        			count += a;
-        		}
+        		count +=(a/10!=0)?(a/10+a%10):a;
         	}else{
         		int a = num[i] - '0';
         		count +=a;
